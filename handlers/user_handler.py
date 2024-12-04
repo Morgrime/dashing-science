8b7f43b7cb97409ef0377376d3657e5fe384bad6
Morgrime <daniel2000@mail.ru>
2024-12-04 11:37:46 +0500

    Added inline kb buttons to choose service

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -1,21 +1,30 @@
-from aiogram import Router
-from aiogram.filters import CommandStart, Command
+from aiogram import Router, types
+from aiogram.filters import CommandStart, Command, StateFilter
+from aiogram.fsm.state import default_state
 from aiogram.types import Message
-from keyboards.inlineKeyboards import support_keyboard
+from keyboards.inlineKeyboards import support_keyboard, service_kb
 
 router = Router()
 
 # /start
-@router.message(CommandStart())
+@router.message(CommandStart(),  StateFilter(default_state))
 async def start_command(message: Message):
     await message.answer('Приветствую, я бот от Лихой Науки\n\n'
                         'Моя задача облегчить вам подбор нужной вам работы, а также её стоимость\n'
                         'Если будут вопросы - напишите команду /help')
     
+# /cancel который не будет работать если ничего не активно
+@router.message(Command(commands='cancel'), StateFilter(default_state))
+async def process_cancel_command(message: Message):
+    await message.answer(
+        text='Отменять нечего, пожалуйста выберите нужную услуга для рассчета стоимости.'
+        'Для этого используйте /calculate'
+    )
+    
 # /help
 @router.message(Command('help'))
 async def help_command(message: Message):
-    await message.answer('1) Нажмите на /choose_service для рассчета стоимости\n'
+    await message.answer('1) Нажмите на /calculate для рассчета стоимости\n'
                          '2) Вам зададут несколько уточняющих вопросов\n'
                          '3) После вопросов вам будет выдана примерная стоимость интересующей вам работы')
 
@@ -24,6 +33,22 @@ async def help_command(message: Message):
 async def support_command(message: Message):
     await message.answer(text='Поддежка пользователя', 
                         reply_markup=support_keyboard)
+
+# /choose - выбрать услугу
+@router.message(Command('calculate'))
+async def choose_service(message: Message):
+    await message.answer(text='Выберите услугу',
+                         reply_markup=service_kb)
+
+# выбрана курсовая    
+@router.callback_query(lambda c: c.data == 'kurs')    
+async def choose_kurs(callback_query: types.CallbackQuery):
+    await callback_query.message.answer('Вы выбрали курсовую, пожалуйста ответьте на несколько вопросов, чтобы я мог оценить вашу работу')
+
+# выбран диплом
+@router.callback_query(lambda c: c.data == 'dipl')    
+async def choose_kurs(callback_query: types.CallbackQuery):
+    await callback_query.message.answer('Вы выбрали диплом, пожалуйста ответьте на несколько вопросов, чтобы я мог оценить вашу работу')
 
 # любое другое сообщение
 @router.message()
