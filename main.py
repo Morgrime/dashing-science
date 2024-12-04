63c2f2cff6ad965855c0602d7a65411093e9b610
Morgrime <daniel2000@mail.ru>
2024-12-04 11:32:21 +0500

    added logger

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
-from config.config import BOT_TOKEN, ADMIN_IDS
+from config.config import BOT_TOKEN
 from handlers.user_handler import router
 from menu.menu  import set_main_menu
+from logger.logger_config import format_1
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO, format=format_1)
 
 async def main():
     dp.include_router(router) # инициализация хэндлеров
