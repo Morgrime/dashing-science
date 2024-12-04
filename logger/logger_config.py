3a13a0300788b3f88f097aab4dfb8f6d2bd08bdc
Morgrime <daniel2000@mail.ru>
2024-12-04 11:32:09 +0500

    Added logger config

---
+++ logger/logger_config.py
@@ -0,0 +1,5 @@
+format_1 = '#%(levelname)-8s [%(asctime)s] - %(filename)s:'\
+           '%(lineno)d - %(name)s - %(message)s'
+
+format_2 = '[{asctime}] #{levelname:8} {filename}:'\
+           '{lineno} - {name} - {message}'
