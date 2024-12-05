e0297afe645d9c4c4c1852e21c52207b62e41197
Morgrime <daniel2000@mail.ru>
2024-12-05 18:26:09 +0500

    Added separate FSM for each service

--- FSM/states.py
+++ FSM/states.py
@@ -1,7 +1,13 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, State, StatesGroup
 
-class CalculatorStates(StatesGroup):
+class KursStates(StatesGroup):
+    theme = State()
+    originality = State() # какой процент оригинальности нужен
+    deadline = State() # сколько дней до дедлайна
+    wishes = State() # пожелания (не обязательно)
+
+class DiplStates(StatesGroup):
     theme = State()
     originality = State() # какой процент оригинальности нужен
     deadline = State() # сколько дней до дедлайна
