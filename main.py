778861a6049b3570cecb4f70ebd13a51ca84cb23
Danil <daniel2000@mail.ru>
2024-12-07 18:11:46 +0500

    Linked dipl_router

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher
 from config.config import BOT_TOKEN
 from handlers.user_handler import router as user_router
 from handlers.kurs_handler import router as kurs_router
+from handlers.dipl_handler import router as dipl_router
 from menu.menu  import set_main_menu
 from logger.logger_config import format_1
 
@@ -16,6 +17,7 @@ logging.basicConfig(level=logging.INFO, format=format_1)
 async def main():
      # инициализация хэндлеров
     dp.include_router(kurs_router)
+    dp.include_router(dipl_router)
     dp.include_router(user_router)
     dp.startup.register(set_main_menu) # инициализация кнопки меню
 
