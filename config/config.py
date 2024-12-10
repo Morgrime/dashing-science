fdf0eb65cd8d3477b8866456f9f606901331602a
Morgrime <daniel2000@mail.ru>
2024-12-10 14:59:06 +0500

    Added setting for db

--- config/config.py
+++ config/config.py
@@ -4,4 +4,8 @@ from environs import Env
 env = Env()
 env.read_env()
 BOT_TOKEN = env("TOKEN")
-# ADMIN_IDS = env("ADMINS")
+DB_NAME = env("DB_NAME")  # Имя базы данных
+DB_USER = env("DB_USER")  # Имя пользователя
+DB_PASS = env("DB_PASS") # Пароль
+DB_HOST = env("DB_HOST")  # Хост сервера PostgreSQL
+DB_PORT = env("DB_PORT")  # Порт сервера PostgreSQL
